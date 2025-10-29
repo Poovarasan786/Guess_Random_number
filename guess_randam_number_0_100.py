5212d471c0311b5ebed4a15e956a244ab8cf9717
Poovarasan786 <poovarasanmurugan786@gmail.com>
2025-10-29 21:41:42 +0530

    this is the starting.

---
+++ guess_randam_number_0_100.py
@@ -0,0 +1,30 @@
+print("guess the number between 0 to 100")
+choice=5
+
+
+import random
+num=random.randint(0,100)
+while choice>0:
+    print()
+    print(" you have only {} guess".format(choice))
+    print()
+    guess=int(input("Enter the number:"))
+    if num==guess:
+        print()
+        print(" ********** you won **************")
+        break
+    elif num>guess:
+        print()
+        print("Too low Guess higher")
+        print("-------------------------")
+    elif num<guess:
+        print("Too high Guess lesser")
+        print("-------------------------")
+    choice-=1
+
+if choice==0:
+    print("you lose")
+    print()
+    print("The answer is {}".formate(num))
+    print()
+    print("Better luck Next time...")
